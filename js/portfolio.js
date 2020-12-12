b9f8056ea9e0f5b8146292ba6479401d502838d1
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-12-12 01:08:46 -0700

    Add Variants and JS for easy btn creation

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,31 +1,70 @@
 console.log("Reading JS");
 $(function() {
-  	// Smooth load in
-	console.log("Doc Loaded");
+  	// Smooth load in ***NOT FULLY IMPLEMENTED***
+	console.log("Document Loaded");
 	var $body = $("body");
 	$body.addClass("smooth-load-transition");
+	
+	findAndCreateButtons();
 
+	setButtonWidthAndHeight();
 
+});
 
-	var $buttonContainers = $(".btn-main--container2");
-	var $buttons = $(".btn-main--container2 > .btn.btn-main2");
-	var $platform = $(".btn-main--container2 > .platform");
-	var $buttonHeight = $(".btn-main--container2 > .button-height");
+// Allows shorthand HTML to be written for buttons.  Contains code that
+// populates HTML withe the markup necessary to display button correctly.
+function findAndCreateButtons() {
+	let $buttonContainers = $(".btn-main--container[data-href]");
+	let variant = "";
+	let url = "";
+
+	// Foreach button container, assign the button variant and write the inner html.
+	$buttonContainers.each(function() {
+		if ($(this).attr("data-variant-success") != undefined) variant = " btn-main--success ";
+		else if ($(this).attr("data-variant-danger") != undefined) variant = " btn-main--danger ";
+
+		url = $(this).attr("data-href") != "" ? "href='" + $(this).attr("data-href") + "'" : ""; 
+
+		$(this).html(
+			  "<a class='btn btn-main" + variant + url
+			+ "' target='_blank'>" + $(this).attr("data-text") + "</a>"
+			+ "<div class='button-height'></div>"
+			+ "<div class='platform'></div>"
+			+ "<div class='platform-wall'></div>");
+	});
+}
+
+// Set the width and height of the components of the button.
+function setButtonWidthAndHeight() {
+	let $buttonContainers = $(".btn-main--container");
+	let $buttons = $(".btn-main--container > .btn.btn-main");
+	let $buttonHeight = $(".btn-main--container > .button-height");
+	let $platforms = $(".btn-main--container > .platform");
+	let $platformWalls = $(".btn-main--container > .platform-wall");
 
 	$buttonContainers.height($buttons.outerHeight());
 	$buttonContainers.width($buttons.outerWidth());
 
-	$buttonHeight.height($buttons.outerHeight());
+	// Set buttonHeight to be 2/3 the size of the button
+	$buttonHeight.height(2 * $buttons.outerHeight() / 3);
 	$buttonHeight.width($buttons.outerWidth());
 
-	$platform.height($buttons.outerHeight() + $platform.height());
-	$platform.width($buttons.outerWidth() + $platform.width());
+	$platforms.height($buttons.outerHeight() + $platforms.height());
+	$platforms.width($buttons.outerWidth() + $platforms.width());
+
+	// Set the platformWalls to be 1/3 the size of the button
+	$platformWalls.height($buttons.outerHeight() / 3);
+	$platformWalls.width($buttons.outerWidth());
 
 	$buttons.on("mousedown", function() {
-		console.log("Button Clicked!");
+		c("Button Clicked!");
 	});
-});
+}
 
+// function to do console.log() (I didn't want to keep writing it out)
+function c(s) {
+	console.log(s);
+}
 
 // Smooth scrolling
 /* $(document).ready(function(){   
