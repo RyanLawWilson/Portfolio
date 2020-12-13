98b5c352d5e8ca759e3f31b20697d8cc6c6bf6c8
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-12-12 19:43:05 -0700

    Simplify JavaScript

--- js/portfolio.js
+++ js/portfolio.js
@@ -11,35 +11,27 @@ $(function() {
 // Allows shorthand HTML to be written for buttons.  Contains code that
 // populates HTML withe the markup necessary to display button correctly.
 function findAndCreateButtons() {
-	let $buttonContainers = $(".btn-main--container[data-href]");
-	let variant = "";
-	let url = "";
-
 	// Foreach button container, assign the button variant and write the inner html.
-	$buttonContainers.each(function() {
+	$(".btn-main--container").each(function() {
+		let variant = "";
 		// Determine which variant to create
 		if ($(this).attr("data-variant-success") != undefined) variant = " btn-main--success ";
 		else if ($(this).attr("data-variant-danger") != undefined) variant = " btn-main--danger ";
 
 		// If the URL is empty, don't bother putting in href=""
 		// Replace all spaces in the text with &nbsp; to fix a bug where each word starts on a new line.
-		url = $(this).attr("data-href") != "" ? "href='" + $(this).attr("data-href") + "'" : ""; 
-		let text = $(this).attr("data-text").replace(/ /g, "&nbsp;")
+		// If the data-text attribute was left out, set text to a default value
+		let url = $(this).attr("data-href") != "" && $(this).attr("data-href") != undefined ? "href='" + $(this).attr("data-href") + "'" : ""; 
+		let text = $(this).attr("data-text") != undefined ? $(this).attr("data-text").replace(/ /g, "&nbsp;") : "Button";
 
-		$(this).html(
-			  "<a class='btn btn-main" + variant + url
-			+ "' target='_blank'>" + text + "</a>"
-			+ "<div class='button-height'></div>"
-			+ "<div class='platform'></div>"
-			+ "<div class='platform-wall'></div>");
+		// Add the components that make up the button to the button container
+		let $button = $("<a class='btn btn-main" + variant + url + "' target='_blank'>" + text + "</a>").appendTo($(this));
+		let $buttonHeight = $("<div class='button-height'></div>").appendTo($(this));
+		let $platform = $("<div class='platform'></div>").appendTo($(this));
+		let $platformWall = $("<div class='platform-wall'></div>").appendTo($(this));
 
 		// These next lines set the width and height of the components of the button.
 
-		let $button = $(this).find(".btn.btn-main");
-		let $buttonHeight = $(this).find(".button-height");
-		let $platform = $(this).find(".platform");
-		let $platformWall = $(this).find(".platform-wall");
-
 		$(this).height($button.outerHeight());
 		$(this).width($button.outerWidth());
 
@@ -54,90 +46,14 @@ function findAndCreateButtons() {
 		$platformWall.height($button.outerHeight() / 3);
 		$platformWall.width($button.outerWidth());
 
+		// On Click Events for buttons
 		$button.on("mousedown", function() {
 			c("Button Clicked!");
 		});
 	});
-}
-
-
-function setButtonWidthAndHeight() {
-	let $buttonContainers = $(".btn-main--container");
-	// var $buttons = $(".btn-main--container > .btn.btn-main");
-
-	$buttonContainers.each(function() {
-		let $button = $(this).find(".btn.btn-main");
-		let $buttonHeight = $(this).find(".button-height");
-		let $platform = $(this).find(".platform");
-		let $platformWall = $(this).find(".platform-wall");
-
-		$(this).height($button.outerHeight());
-		$(this).width($button.outerWidth());
-
-		// Set buttonHeight to be 2/3 the size of the button
-		$buttonHeight.height(2 * $button.outerHeight() / 3);
-		$buttonHeight.width($button.outerWidth());
-
-		$platform.height($button.outerHeight() + $platform.height());
-		$platform.width($button.outerWidth() + $platform.width());
-
-		// Set the platformWalls to be 1/3 the size of the button
-		$platformWall.height($button.outerHeight() / 3);
-		$platformWall.width($button.outerWidth());
-
-		$button.on("mousedown", function() {
-			c("Button Clicked!");
-		});
-	});
-
-	// $buttonContainers.height($buttons.outerHeight());
-	// $buttonContainers.width($buttons.outerWidth());
-
-	// // Set buttonHeight to be 2/3 the size of the button
-	// $buttonHeight.height(2 * $buttons.outerHeight() / 3);
-	// $buttonHeight.width($buttons.outerWidth());
-
-	// $platforms.height($buttons.outerHeight() + $platforms.height());
-	// $platforms.width($buttons.outerWidth() + $platforms.width());
-
-	// // Set the platformWalls to be 1/3 the size of the button
-	// $platformWalls.height($buttons.outerHeight() / 3);
-	// $platformWalls.width($buttons.outerWidth());
-
-	// $buttons.on("mousedown", function() {
-	// 	c("Button Clicked!");
-	// });
 }
 
 // function to do console.log() (I didn't want to keep writing it out)
 function c(s) {
 	console.log(s);
 }
-
-// Smooth scrolling
-/* $(document).ready(function(){   
-    $("a").on('click', function(event) {
-        
-    if (this.Link !== "") {
-        event.preventDefault();
-
-        var Link = this.Link;
-
-        $('html, body').animate({
-            scrollTop: $(Link).offset().top
-        }, 2000, function(){
-            window.location.Link = Link;
-        });
-    }
-    });
-  });
-  
-  function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-    this.Close();
-  } */
-  
